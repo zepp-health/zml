f4717d4da5a225b6c742ba990b3d3d8aaa81e0eb
htoo <ht.anglenx@gmail.com>
2023-10-29 08:37:20 +0800

    feat |> example about sync data

---
+++ examples/syncData/app.js
@@ -0,0 +1,14 @@
+import { BaseApp } from '@zeppos/zml/base-app'
+import { log } from '@zos/utils'
+const logger = log.getLogger('sync-data-1.0')
+
+App(
+  BaseApp({
+    globalData: {
+    },
+    onCreate() {
+    },
+    onDestroy(opts) {
+    },
+  }),
+)
