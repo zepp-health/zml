4927937a50d3d350473b36eab9253de7733e6126
htoo <ht.anglenx@gmail.com>
2023-11-08 00:37:22 +0800

    feat |> add queryString lib

---
+++ src/core/common/qs.js
@@ -0,0 +1,27 @@
+import { _r } from './common'
+
+export function stringify(obj) {
+  if (!obj || typeof obj !== 'object') {
+    throw new Error('obj is required and must be an object')
+  }
+
+  const parts = []
+
+  for (let [key, value] of Object.entries(obj)) {
+    key = encodeURIComponent(key)
+
+    if (Array.isArray(value)) {
+      // handle array
+      value.forEach((item) => {
+        parts.push(`${key}[]=${encodeURIComponent(item)}`)
+      })
+    } else {
+      // handle regular value
+      parts.push(`${key}=${encodeURIComponent(value)}`)
+    }
+  }
+
+  return parts.join('&')
+}
+
+export const parse = _r('@zos/utils').qs.parse
