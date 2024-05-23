b9eeb9cdc7776d6ec48378a8dea2194a08683f83
htoo <ht.anglenx@gmail.com>
2024-05-23 12:39:39 +0800

    ref |> example always use zml@latest

--- examples/helloworld3/app.js
+++ examples/helloworld3/app.js
@@ -9,14 +9,14 @@ App(
       this.globalData.a = 2
       this.log(this.globalData.a)
       this.log('app test')
-      this.test()
-      this.test2()
+      // this.test()
+      // this.test2()
     },
     onDestroy(opts) {
-      this.globalData.a += 1
-      this.globalData.a += 2
-      this.log('destroy=2',this.globalData.a)
-      this.log('app on destroy invoke')
+      // this.globalData.a += 1
+      // this.globalData.a += 2
+      // this.log('destroy=2',this.globalData.a)
+      // this.log('app on destroy invoke')
     },
     test() {
       this.globalData.a += 1
