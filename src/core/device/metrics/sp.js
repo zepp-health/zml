763cd404c1a87825c1d19674867a62aba7d62c02
hangxuan <hangxuan@zepp.com>
2025-01-16 19:41:59 +0800

    ref |> spx方法增加扩展sp方法

--- src/core/device/metrics/sp.js
+++ src/core/device/metrics/sp.js
@@ -2,7 +2,9 @@ import { ui, _px } from '../../common/common.js'
 
 function spx(fz) {
   // 首次调用时进行判断，并缓存结果到 sp 函数自身
-  spx = ui.getSysFontSize ?? (s => s);
+  const _spx = ui.getSysFontSize ?? (s => s);
+  spx = _spx;
+  spx.sp = _spx;
   // 调用缓存的函数
   return spx(_px(fz));
 }
