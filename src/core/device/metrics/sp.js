0e443c1b0e2a0199560a2c69a59db5b38c20c1e1
huangtao1 <huangtao1@zepp.com>
2025-04-09 17:35:15 +0800

    fix |> replace sp function

--- src/core/device/metrics/sp.js
+++ src/core/device/metrics/sp.js
@@ -1,12 +1,12 @@
 import { ui } from '../../common/common.js'
 
-const _spx = ui.getSysFontSize ?? (s => s);
+const _dpi_sp = ui.sp ?? ((s) => s)
 
 function sp(num) {
   return {
     [Symbol.toPrimitive](hint) {
-      return (hint === 'string') ? `${num.toFixed(0)}sp` : _spx(num);
-    }
+      return hint === 'string' ? `${num.toFixed(0)}sp` : _dpi_sp(num)
+    },
   }
 }
 
